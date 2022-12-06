ce19d9d643eb3f4c3e01c2c75dd031d7020fed91
CoDrift <co.drift07@gmail.com>
2022-12-06 17:20:58 +0500

    I tried

--- main.py
+++ main.py
@@ -4,22 +4,36 @@ from tkinter import *
 from tkinter import filedialog
 
 # Get local file
-media_player = vlc.MediaPlayer()
+localMedia = vlc.MediaPlayer()
+networkMedia = vlc.MediaPlayer()
 filepath = filedialog.askopenfilename(initialdir="C:\\Users",
                                           title="Open file okay?",
                                           filetypes= (("media","*.mp4"),
                                           ("all files","*.*")))
 
 # Getting stream data
-streamLoc = str(input("Enter stream location: "))
-print(streamLoc)
+# streamLoc = str(input("Enter stream location: "))
+# print(streamLoc)
+
+# Network Player
+Media = vlc.Media("mmsh://127.0.0.1:8080")
+networkMedia.set_media(Media)
+networkMedia.play()
 
 # Starting the player
 media = vlc.Media(filepath)
-media_player.set_media(media)
-media_player.play()
+localMedia.set_media(media)
+localMedia.play()
 sleep(5)
 
+networkMedia.audio_set_volume(0)
+networkMedia.video_set_scale(0.1)
+localMedia.set_position(0.5)
+localMedia.audio_set_volume(1)
+
 # Player Controls after this
-while media_player.is_playing():
-     sleep(1)
+while localMedia.is_playing():
+     # sleep(2)
+     print(networkMedia.get_position())
+     print(localMedia.get_position())
+     # localMedia.set_position(networkMedia.get_position())
